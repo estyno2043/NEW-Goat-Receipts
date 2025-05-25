a4f7688e185b882e1a284a47d5f679dcd5466391
estyno2043 <kuboestok@gmail.com>
2025-05-25 20:59:38 +0000

    Restored to '7734253eec7e9fe8ce6b2d1f7886bdb2002d8e9e'
    Replit-Restored-To: 7734253eec7e9fe8ce6b2d1f7886bdb2002d8e9e

--- modals/coolblue.py
+++ modals/coolblue.py
@@ -164,7 +164,8 @@ class coolbluemodal(ui.Modal, title="discord.gg/goatreceipts"):
             except Exception as e:
                 embed = discord.Embed(title="Error", description=f"An error occurred: {str(e)}")
                 await interaction.edit_original_response(embed=embed)
-        except Exception as e:
+
+        else:
             # Handle case where no user details are found
             embed = discord.Embed(title="Error", description="No user details found. Please ensure your information is set up.")
             await interaction.response.send_message(embed=embed, ephemeral=True)
